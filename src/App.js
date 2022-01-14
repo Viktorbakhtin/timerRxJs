249cc3e4c86ebbd24bd08a88a615d76e39c7105a
VIktorBakhtin <viktorbakhtin@gmail.com>
2022-01-14 19:48:39 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { useEffect, useState } from "react";
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 function App() {
+
+
+  
+  const [sec, setSec] = useState(0);
+  const [status, setStatus] = useState("stop");
+  const [count, setCount] = useState(1);
+  console.log("fdsfdf",count)
+
+ 
+  useEffect(() => {
+    const unsub = new Subject();
+    interval(1000)
+      .pipe(takeUntil(unsub)).subscribe(() => {
+        if (status === "run") {
+          setSec(a => a + 1000);
+        } 
+      });
+    return () => {
+       unsub.next();
+       unsub.complete();
+    };
+  }, [status]);
+ 
+  const start = React.useCallback(() => {
+    setStatus("run");
+  }, []);
+ 
+  const stop = React.useCallback(() => {
+    setStatus("stop");
+    setSec(0);
+  }, []);
+ 
+  const reset = React.useCallback(() => {
+    setSec(0);
+  }, []);
+ 
+  const wait = React.useCallback(() => {
+    increment()
+    setStatus("wait");
+  }, []);
+
+  const increment = React.useCallback(() => {
+    setCount(count + 1);
+    console.log(count,"внизху")
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Вы кликнули {count} раз(а)</p>
+      <span> {new Date(sec).toISOString().slice(11, 19)}</span>
+      <button className="start-button" onClick={start}>
+        Start
+      </button>
+      <button className="stop-button" onClick={stop}>
+        Stop
+      </button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={wait}>Wait</button>
     </div>
   );
 }
